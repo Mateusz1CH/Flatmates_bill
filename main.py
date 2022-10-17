e7b6746361759ef78c90271da1c8a28d4b4e2df0
Mateo <mateusz1cholewa@gmail.com>
2022-10-18 01:03:04 +0200

    create separate module Report.py for pdf generator function

--- main.py
+++ main.py
@@ -1,50 +1,5 @@
-import webbrowser
-
-from fpdf import FPDF
-
 from flat import Bill, Flatmate
-
-
-class PdfReport:
-    """
-    Create a Pdf file that contain data about the flatmates such as their name, their due amount and the period of the bill
-    """
-
-    def __init__(self, filename):
-        self.filename = filename
-
-    def generate(self, flatmate1, flatmate2, bill):
-
-        flatmate1_pay = str(round(flatmate1.pays(bill=bill, flatmate2=flatmate2), 2))
-        flatmate2_pay = str(round(flatmate2.pays(bill=bill, flatmate2=flatmate1), 2))
-
-        pdf = FPDF(orientation='P', unit='pt', format='A4')
-        pdf.add_page()
-
-        # Add icon
-        pdf.image("C:/Users/Mateo/PycharmProjects/files-master/App-2-Flatmates-Bill/files/house.png", w=30, h=30)
-
-        # Add title
-        pdf.set_font(family='Times', size=24, style='B')
-        pdf.cell(w=0, h=80, txt='Flatmates Bill', border=1, align='C', ln=1)
-
-        # Insert period label and value
-        pdf.set_font(family='Times', size=14, style='B')
-        pdf.cell(w=100, h=40, txt='Period:', border=0)
-        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
-
-        # Insert name and amount of the first flatmate
-        pdf.set_font(family='Times', size=12)
-        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
-        pdf.cell(w=150, h=25, txt=flatmate1_pay, border=0, ln=1)
-
-        # Insert name and amount of the second flatmate
-        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
-        pdf.cell(w=150, h=25, txt=flatmate2_pay, border=0, ln=1)
-
-        pdf.output(self.filename)
-
-        webbrowser.open(self.filename)
+from report import PdfReport
 
 bill_amount = float(input("Buenos dias, enter the bill amount: "))
 bill_period = input("What is the bill period? E.g December 2022 ")
@@ -62,5 +17,5 @@ second_person = Flatmate(name = name2, days_in_house = days_in_house2)
 print(f"{first_person.name}", first_person.pays(bill=the_bill, flatmate2 = second_person))
 print(f"{second_person.name}", second_person.pays(bill=the_bill, flatmate2 = first_person))
 
-pdf_report = PdfReport(filename = f"{the_bill.period}.pdf")
+pdf_report = PdfReport(filename =f"{the_bill.period}.pdf")
 pdf_report.generate(flatmate1=first_person, flatmate2=second_person, bill=the_bill)
