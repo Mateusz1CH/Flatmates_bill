4801db7a3ed41e16fd9452b586a7cf868999ba5b
Mateo <mateusz1cholewa@gmail.com>
2022-10-17 23:22:21 +0200

    added picture and changed font

--- main.py
+++ main.py
@@ -39,14 +39,20 @@ class PdfReport:
         pdf = FPDF(orientation='P', unit='pt', format='A4')
         pdf.add_page()
 
+        # Add icon
+        pdf.image("C:/Users/Mateo/PycharmProjects/files-master/App-2-Flatmates-Bill/files/house.png", w=30, h=30)
+
         # Add title
         pdf.set_font(family='Times', size=24, style='B')
         pdf.cell(w=0, h=80, txt='Flatmates Bill', border=1, align='C', ln=1)
+
         # Insert period label and value
+        pdf.set_font(family='Times', size=14, style='B')
         pdf.cell(w=100, h=40, txt='Period:', border=0)
         pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
 
         # Insert name and amount of the first flatmate
+        pdf.set_font(family='Times', size=12)
         pdf.cell(w=100, h=40, txt=flatmate1.name, border=0)
         pdf.cell(w=150, h=40, txt=flatmate1_pay, border=0, ln=1)
 
